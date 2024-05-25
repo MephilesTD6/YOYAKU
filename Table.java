904d0596986f637813786509334ea54d94e1ec18
Masterninja07 <fadhlsyukriayie@gmail.com>
2024-05-25 16:25:18 +0800

    Syukri Fadhli (24000074)

---
+++ Table.java
@@ -0,0 +1,25 @@
+//Syukri Fadhli bin Ahmad
+public class Table {
+
+    public int TableNumber() {
+        int number = 0;
+
+        return number;
+    }
+
+    public int ChairNumber() {
+        int number = 0;
+
+        return number;
+    }
+
+    public boolean ReservationStatus() {
+        boolean status = false;
+
+        return status;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

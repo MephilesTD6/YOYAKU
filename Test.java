794b6be05b25a78b7adb43b9cb29eac27c3cb3c3
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-05-25 11:01:52 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Tables {
+
+    int[] tables = {1, 0, 1, 1, 0, 1};
+
+    public int addTable() {
+        return 1;
+    }
+
+    public int removeTable() {
+        return -1;
+    }
+
+    public void print() {
+        int sum = 0;
+        System.out.print("\nTotal tables: ");
+        
+        for (int i = 0; i < tables.length; i++)
+            sum += tables[i];
+        
+        System.out.println(sum);
+    }
+
+    public int find() {
+        Scanner scanner = new Scanner(System.in);
+        int i;
+        
+        System.out.print("Enter table number: ");
+        i = scanner.nextInt();
+        
+        return tables[i];
+    }
+}
+
+    public static void main(String[] args) {
+        Tables ashwin = new Tables();
+
+        System.out.println(ashwin.addTable());
+        System.out.println(ashwin.removeTable());
+        ashwin.print();
+        System.out.println(ashwin.find());
+    }

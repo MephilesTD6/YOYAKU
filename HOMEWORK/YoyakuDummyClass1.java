fde53cc4d0968bfe75b0e97f7f14de59b29a7131
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-05-25 17:41:01 +0800

    Update YoyakuDummyClass1.java

--- HOMEWORK/YoyakuDummyClass1.java
+++ HOMEWORK/YoyakuDummyClass1.java
@@ -2,19 +2,21 @@ package Java.Homework;
 
 public class YoyakuDummyClass1
 {
-    public static void client() // Avinash Kumar A/L Jayaseelan (24000113)
+    public static void client(String name[]) // Avinash Kumar A/L Jayaseelan (24000113)
     {
         System.out.println("client method exists and can be called");
         return;
     }
-    public static void reservations() // Avinash Kumar A/L Jayaseelan (24000113)
+    public static void reservations(String phone[]) // Avinash Kumar A/L Jayaseelan (24000113)
     {
         System.out.println("reservations method exists and can be called");
         return;
     }
     public static void main(String []args)
     {
-        client();
-        reservations();
+        String name[] = new String[3];
+        String phone[] = new String[3];
+        client(name);
+        reservations(phone);
     }
 }

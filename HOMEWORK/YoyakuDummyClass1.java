77fdfac25affd5797b94c0598d7933a71a3a65c7
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-05-25 17:30:18 +0800

    Add files via upload

---
+++ HOMEWORK/YoyakuDummyClass1.java
@@ -0,0 +1,20 @@
+package Java.Homework;
+
+public class YoyakuDummyClass1
+{
+    public static void client() // Avinash Kumar A/L Jayaseelan (24000113)
+    {
+        System.out.println("client method exists and can be called");
+        return;
+    }
+    public static void reservations() // Avinash Kumar A/L Jayaseelan (24000113)
+    {
+        System.out.println("reservations method exists and can be called");
+        return;
+    }
+    public static void main(String []args)
+    {
+        client();
+        reservations();
+    }
+}

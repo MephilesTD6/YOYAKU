054622eef29b2183bb614191362073726926fc1a
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-05-29 18:48:06 +0800

    5) Reservation

---
+++ Reservaction.java
@@ -0,0 +1,29 @@
+//Avinash Kumar A/L Jayaseelan
+
+package Java.Homework;
+
+public class Reservaction {
+
+    private String name;
+    private String phone_number;
+
+    public void add()
+    {
+        return;
+    }
+
+    public void delete()
+    {
+        return;
+    }
+
+    public void print()
+    {
+        return;
+    }
+
+    public void find()
+    {
+        return;
+    }
+}

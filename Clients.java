88c68bb72498ede7bb6d833a15ba47502b986dae
Sennnnnnnnnnnn <112443226+Sennnnnnnnnnnn@users.noreply.github.com>
2024-05-29 19:34:57 +0800

    Create Clients.java

---
+++ Clients.java
@@ -0,0 +1,25 @@
+//Hamzah Muhsin bin Hafiz Al-Asadi
+
+public class Clients{
+
+    private String[] Clientname;
+
+
+
+    public void addClient() {
+    
+    }
+
+    public void removeClient() {
+
+    }
+
+    public void printClient() {
+
+    }
+
+    public void findClient() {
+        
+
+    }
+}

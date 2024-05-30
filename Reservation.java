ecf255d3fd0aeb272169fd15e1e8c9c48bb373a0
rockyash1823 <146174105+rockyash1823@users.noreply.github.com>
2024-05-31 01:26:56 +0800

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -6,13 +6,11 @@ public class Reservation {
     
     public int ask_table_num()
     {
-        
         return 0;
     }
     
     public int count_number()
     {
-        
         return 0;
     }
     

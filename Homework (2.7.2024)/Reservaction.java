a8253878388d44af5cbb61771f10f8e3bcd7bf97
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-06-02 13:49:45 +0800

    Add files via upload

---
+++ Homework (2.7.2024)/Reservaction.java
@@ -0,0 +1,33 @@
+//Avinash Kumar A/L Jayaseelan
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reservaction {
+
+ private List<Reservation> reservaction;
+
+    public Reservaction() {
+    this.reservaction = new ArrayList<>();
+    }
+
+    public void addRes()
+    {
+        return;
+    }
+
+    public void removeRes()
+    {
+        return;
+    }
+
+    public void printRes()
+    {
+        return;
+    }
+
+    public void findRes()
+    {
+        return;
+    }
+}
---
+++ Homework (2.7.2024)/Table.java
@@ -0,0 +1,24 @@
+//Syukri Fadhli bin Ahmad
+import java.util.Scanner;
+
+public class Table {
+
+    private int tableNumber;
+    private int chairNumber;
+    
+     Scanner scanner = new Scanner(System.in);
+    
+    public int add_table_num() {
+        return tableNumber;
+    }
+
+    public int add_chair_num() {
+        return chairNumber;
+    }
+
+    public boolean reservation_status() {
+        boolean status = false;
+
+        return status;
+    }
+}

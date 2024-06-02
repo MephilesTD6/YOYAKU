ba216581426987b8e103ece47da40546eaf786b4
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-06-02 11:04:53 +0800

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -3,17 +3,16 @@
 import java.util.Scanner;
 
 public class Reservation {
-    
+
+  private int tableNumber;
   private int number_of_persons;
   
    Scanner scanner = new Scanner(System.in);
    
-   /*removed as when customers make reservations do not give the desired table number
-   
    public int get_table_num()
     {
         return 0;
-    }*/
+    }
 
     //input number of persons
     public int get_pax();

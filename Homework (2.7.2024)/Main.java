a42ff49fd6c96ab880bfc5d6452a972de2bd216a
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-06-06 20:45:01 +0800

    Update Main.java

--- Homework (2.7.2024)/Main.java
+++ Homework (2.7.2024)/Main.java
@@ -1,5 +1,6 @@
 // See Tho Soon Yinn
 // 24000187
+import java.util.*;
 
 public class Main
 {
@@ -11,17 +12,36 @@ public class Main
 		Reservaction avi = new Reservaction();
 		Table syukri = new Table();
 		Tables seetho = new Tables();
-		
-		avinash.get_name();
-		avinash.get_phone_num();
-			
-            	hamzah.addClient();
-            	hamzah.removeClient();
-            	hamzah.printClient();
-            	hamzah.findClient();
 
-		ashwin.get_table();
-		ashwin.get_pax();
+		Scanner scan = new Scanner(System.in);
+
+		avinash.set_name();
+		avinash.set_phone_number();
+		System.out.println("Enter client name: " + avinash.get_name(String name));
+		System.out.println("Enter client contact number: " + avinash.get_phone_num(String num));
+			
+            	System.out.println("Do you wish to add a client? (Y/N)");
+		addClient = scan.nextChar();	
+			if(addClient == 'Y')
+			hamzah.addClient();
+
+		System.out.println("Do you wish to remove a client? (Y/N)");
+		removeClient = scan.nextChar();	
+			if(removeClient == 'Y')
+            		hamzah.removeClient();
+
+		System.out.println("Do you wish to print client details? (Y/N)");
+		printClient = scan.nextChar();	
+			if(printClient == 'Y')
+            		hamzah.printClient();
+
+		System.out.println("Do you wish to find a client? (Y/N)");
+		findClient = scan.nextChar();
+			if(findClient == 'Y')
+            		hamzah.findClient();
+
+		System.out.println("Enter table number: "+ ashwin.get_table());
+		System.out.println("Enter number of clients: "+ ashwin.get_pax());
 		ashwin.client_input();
             
             	avi.addRes();
@@ -29,9 +49,9 @@ public class Main
             	avi.printRes();
             	avi.findRes();
 
-		syukri.get_table_num();
-		syukri.get_chair_num();
-		syukri.reservation_status();
+		System.out.println("Enter table number: " + syukri.get_table_num());
+		System.out.println("Enter the number of chairs: "+ syukri.get_chair_num());
+		System.out.println("Reservation status: "+ syukri.reservation_status());
 		
             	seetho.addTable();
             	seetho.removeTable();

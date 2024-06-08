4a71dd2f721af106dc3a771cfa7ceb1c80fb662e
Masterninja07 <fadhlsyukriayie@gmail.com>
2024-06-08 18:51:04 +0800

    unfinished

--- Clients.java
+++ Clients.java
@@ -4,20 +4,38 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Clients {
+    String name, phoneNumber;
+
+    Scanner input = new Scanner(System.in);
 
     private ArrayList<Client> clientDatabase;
 
+    // CONSTRUCTORS
     public Clients() {
-        this.clientDatabase = new ArrayList<Client>();
+        this.clientDatabase = new ArrayList<>();
     }
 
     public void addClient() {
+        Client newClient = new Client(name, phoneNumber);
+        newClient.setName();
+        newClient.setPhoneNumber();
+        clientDatabase.add(newClient);
+        System.out.println("Client added successfully.");
+    }
+
+    // DEBUG ZONE
+    public void display() {
+        System.out.println(clientDatabase.get(0));
 
     }
 
     public void removeClient() {
+        System.out.print("Enter client name to be removed: ");
+        String name = input.nextLine();
+        boolean isRemoved = false;
 
     }
 

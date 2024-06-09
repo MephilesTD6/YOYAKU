c9afacb8f67f2aa5c66a778412ac7270b45884d1
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-06-09 14:30:45 +0800

    Update Main.java

--- Homework (12.7.2024)/Main.java
+++ Homework (12.7.2024)/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main (String[]args) {
  
         Scanner sc = new Scanner (System.in);
-        char addT;
+        char addT,removeT,printT,findT;
+        int indexNumber;
 
         /*Client avinash = new Client();
         
@@ -15,13 +16,38 @@ public class Main {
         avinash.getPhoneNumber();
         System.out.println("The client's name is : " + avinash.getName());
         System.out.println("The client's contact number is: " + avinash.getPhoneNumber()); */ 
+        
 
-        Tables seetho = new Tables ();
+        Tables seetho = new Tables();
 
         System.out.println("Do you want to add a table into the system? (Y/N)");
-        addT = sc.next ().charAt (0);
-            if (addT == 'Y') 
-				seetho.addTable(); 
-				
+        addT = sc.next().charAt(0);
+        if (addT == 'Y' || addT == 'y') {
+            seetho.addTable();
+        }
+
+        System.out.println("Do you want to remove a table from the system? (Y/N)");
+        removeT = sc.next().charAt(0);
+        if (removeT == 'Y' || removeT == 'y') {
+            System.out.println("Enter table index number to remove: ");
+            indexNumber = sc.nextInt();
+            seetho.removeTable(indexNumber);
+        }
+
+        System.out.println("Do you want to print the table list from the system? (Y/N)");
+        printT = sc.next().charAt(0);
+        if (printT == 'Y' || printT == 'y') {
+            seetho.printTable();
+        }
+
+        System.out.println("Do you want to find a table in the system? (Y/N)");
+        findT = sc.next().charAt(0);
+        if (findT == 'Y' || findT == 'y') {
+            System.out.println("Enter table index number: ");
+            indexNumber = sc.nextInt();
+            seetho.findTable(indexNumber);
+        }
+
+        sc.close();
     }
 }

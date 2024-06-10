b42f9d4f2082a489e28e3d3df0663a48f1af3f01
Masterninja07 <fadhlsyukriayie@gmail.com>
2024-06-10 17:54:48 +0800

    test for reservation and table

--- Homework (12.7.2024)/Main.java
+++ Homework (12.7.2024)/Main.java
@@ -21,6 +21,50 @@ public class Main {
          * avinash.getPhoneNumber());
          */
 
+        // TESTING CLIENT CLASS
+
+        // TESTING CLIENTS CLASS
+
+        // TESTING RESERVATION CLASS
+        Reservation ashwin1 = new Reservation();// constructor with no parameter
+        ashwin1.get_table();
+        ashwin1.get_pax();
+        System.out.println(ashwin1);
+
+        Reservation ashwin2 = new Reservation(4, 20);
+        System.out.println(ashwin2);
+
+        // TESTING RESERVATIONS CLASS
+        Reservations avinash = new Reservations();
+
+        System.out.println("\nDo you want to add a reservation into the system? (Y/N)");
+        addT = sc.next().charAt(0);
+        if (addT == 'Y' || addT == 'y') {
+            avinash.addRes(ashwin2);
+        }
+
+        System.out.println("Do you want to remove a reservation from the system? (Y/N)");
+        removeT = sc.next().charAt(0);
+        if (removeT == 'Y' || removeT == 'y') {
+            System.out.println("Enter reservation index number to remove: ");
+            indexNumber = sc.nextInt();
+            avinash.removeRes(indexNumber);
+        }
+
+        System.out.println("Do you want to print the reservation list from the system? (Y/N)");
+        printT = sc.next().charAt(0);
+        if (printT == 'Y' || printT == 'y') {
+            avinash.printRes();
+        }
+
+        System.out.println("Do you want to find a reservation in the system? (Y/N)");
+        findT = sc.next().charAt(0);
+        if (findT == 'Y' || findT == 'y') {
+            System.out.println("Enter reservation index number: ");
+            indexNumber = sc.nextInt();
+            avinash.findRes(indexNumber);
+        }
+
         // TESTING TABLE CLASS
         Table syukri1 = new Table();// test constructor with no parameter
         syukri1.setTableNum();
@@ -34,7 +78,8 @@ public class Main {
 
         // TESTING TABLES CLASS
         Tables seetho = new Tables();
-        System.out.println("Do you want to add a table into the system? (Y/N)");
+
+        System.out.println("\nDo you want to add a table into the system? (Y/N)");
         addT = sc.next().charAt(0);
         if (addT == 'Y' || addT == 'y') {
             seetho.addTable(syukri1);

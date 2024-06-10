5bfc2a9ec8859546bc2389f10776f18291f28b12
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-06-10 14:37:00 +0800

    Update Table.java

--- Homework (12.7.2024)/Table.java
+++ Homework (12.7.2024)/Table.java
@@ -11,46 +11,61 @@ public class Table {
 
     private int tableNumber;
     private int chairNumber;
+    
+    boolean status = false;
+    
+     Scanner sc = new Scanner(System.in);
+     
+     public Table(int tableNumber, int chairNumber){
+         this.tableNumber = tableNumber;
+         this.chairNumber = chairNumber;
+         System.out.println("\nCreated a Table object ");
+     }
 
-    Scanner input = new Scanner(System.in);
-
-    // CONSTRUCTOR
-    public Table(int tableNumber, int chairNumber) {
-        this.tableNumber = tableNumber;
-        this.chairNumber = chairNumber;
-        System.out.println("\nCreated a Table object ");
-    }
-
-    public Table() {
-        this.tableNumber = tableNumber;
-        this.chairNumber = chairNumber;
-    }
-
-    // SETTERS
+     public Table(){
+         this.tableNumber = tableNumber;
+         this.chairNumber = chairNumber;
+         System.out.println("\nCreated a Table object ");
+     }
+    
+    
     public void setTableNum() {
-        System.out.print("Enter table number: ");
-        this.tableNumber = input.nextInt();
+        System.out.println("Enter table number: ");
+        this.tableNumber = sc.nextInt();
     }
-
+    
     public void setChairNum() {
-        System.out.print("Enter number of chair: ");
-        this.chairNumber = input.nextInt();
+        System.out.println("Enter number of chair: ");
+        this.chairNumber = sc.nextInt();
     }
-
-    // GETTERS
+    
     public int getTableNum() {
         System.out.println("\nThe table number is: " + tableNumber);
         return tableNumber;
     }
 
     public int getChairNum() {
-        System.out.println("\nThe chair numbe is: " + chairNumber);
+        System.out.println("\nThe number of chairs is: " + chairNumber);
         return chairNumber;
     }
 
-    public boolean reservation_status() {
+    public boolean reservationStatus() {
         boolean status = false;
 
         return status;
+    }
+    
+    //To translate output to human friendly language, if not used, output from Tables would be printed as Table@1a2b3c4d
+    /*Another way to code would to be:
+    
+       public String printDetails() {
+        return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber;
+    }
+    
+    then, when calling ArrayList actions: use for example, seetho.get(indexNumber).printDetails();
+    */
+        @Override
+    public String toString() {
+        return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber + ", Reservation status: " + status;
     }
 }

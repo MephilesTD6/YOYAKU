0a546f5704a0968de14737ee53b4a36cb56ae1ea
Masterninja07 <fadhlsyukriayie@gmail.com>
2024-06-10 17:53:50 +0800

    bug fix

--- Homework (12.7.2024)/Reservations.java
+++ Homework (12.7.2024)/Reservations.java
@@ -3,31 +3,26 @@
 
 import java.util.*;
 
-
 public class Reservations {
 
-    //Create an array from Reservation class
+    // Create an array from Reservation class
     private ArrayList<Reservation> reservations;
 
-    //constructor for code fluidity
+    // constructor for code fluidity
     public Reservations() {
-    this.reservations = new ArrayList<>();
+        this.reservations = new ArrayList<>();
     }
-    
-    public void addRes(Reservation res) 
-    {
+
+    public void addRes(Reservation res) {
         reservations.add(res);
         System.out.println("New reservation added!");
     }
 
-    public void removeRes(int i) {
-        if (indexNumber >= 0 && indexNumber < reservations.size()) 
-        {
-            reservations.remove(i);
+    public void removeRes(int indexNumber) {
+        if (indexNumber >= 0 && indexNumber < reservations.size()) {
+            reservations.remove(indexNumber);
             System.out.println("Reservation removed successfully");
-        }
-        else 
-        {
+        } else {
             System.out.println("Invalid reservation");
         }
     }
@@ -43,12 +38,9 @@ public class Reservations {
     }
 
     public void findRes(int i) {
-        if (i >= 0 && i < reservations.size())
-        {
-             System.out.println("Index " + i + ": " + reservations.get(i));
-        }
-        else
-        {
+        if (i >= 0 && i < reservations.size()) {
+            System.out.println("Index " + i + ": " + reservations.get(i));
+        } else {
             System.out.println("No reservations found");
         }
     }

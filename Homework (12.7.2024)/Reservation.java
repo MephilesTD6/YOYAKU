3ba8a8dc4cdff18279d0cd4f9fdc2350164534f9
Masterninja07 <fadhlsyukriayie@gmail.com>
2024-06-10 17:53:16 +0800

    bug fix

--- Homework (12.7.2024)/Reservation.java
+++ Homework (12.7.2024)/Reservation.java
@@ -5,43 +5,46 @@ import java.util.Scanner;
 
 public class Reservation {
 
-  private int tableNumber;
-  private int number_of_persons;
-  
-   private Scanner scanner = new Scanner(System.in);
-   
-   public Reservation(int tableNumber, int number_of_persons)
-   {
-       this.tableNumber = tableNumber;
-       this.number_of_persons = number_of_persons
-       System.out.println("\nCreated a reservation object with table number" + tableNumber + "for" + number_of_persons +"persons.")
-       
-   }
-   public Reservation()
-   {
-       this.tableNumber = 0;
-       this.number_of_persons = 0;
-   }
-   public void get_table()
-    {
-        System.out.print("Enter table number: ");
-        this.tableNumber = scanner.nextInt()
-        
+    private int tableNumber;
+    private int number_of_persons;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public Reservation(int tableNumber, int number_of_persons) {
+        this.tableNumber = tableNumber;
+        this.number_of_persons = number_of_persons;
+        System.out
+                .println("\nCreated a reservation object with table number " + tableNumber + " for " + number_of_persons
+                        + " persons.");
+
     }
 
-    //input number of persons
-    public void get_pax();
-    {
-        System.out.print("Enter number of persons: ");
-        this.numberOfPersons = scanner.nextInt();
+    public Reservation() {
+        this.tableNumber = 0;
+        this.number_of_persons = 0;
+        System.out.println("\nCreated reservation object");
     }
-    
-    //read input from Client class
-    public void client_input() 
-    {
+
+    public void get_table() {
+        System.out.print("Enter table number: ");
+        this.tableNumber = scanner.nextInt();
+
+    }
+
+    // input number of persons
+    public void get_pax() {
+        System.out.print("Enter number of persons: ");
+        this.number_of_persons = scanner.nextInt();
+    }
+
+    // read input from Client class
+    public void client_input() {
         get_pax();
         get_table();
-        
     }
- 
+
+    @Override
+    public String toString() {
+        return "Table number: " + tableNumber + " Number of person: " + number_of_persons;
+    }
 }

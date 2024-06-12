87aac413ede2460df6835ace291bbc3dd7caba20
delmarcusAK1104 <147365127+delmarcusAK1104@users.noreply.github.com>
2024-06-12 21:24:10 +0800

    Update Reservations.java

--- Homework (12.7.2024)/Reservations.java
+++ Homework (12.7.2024)/Reservations.java
@@ -13,13 +13,13 @@ public class Reservations {
     }
     // Method to add reservation to list of reservations 
     public void addRes(Reservation ashwin) {
-        reservations.add(ashwin);
+        reservations.add(ashwin); // appends ashwin to array list of reservations
         System.out.println("New reservation added!");
     }
     // Method to remove reservation from list of reservations
     public void removeRes(int resIndex) {
         if (resIndex >= 0 && resIndex < reservations.size()) {
-            reservations.remove(resIndex);
+            reservations.remove(resIndex); // removes reservation from array list of reservations using the index
             System.out.println("Reservation removed successfully");
         } else {
             System.out.println("Invalid reservation");
@@ -27,20 +27,20 @@ public class Reservations {
     }
     // Method to print all reservations in list 
     public void printRes() {
-        if (reservations.isEmpty()) {
+        if (reservations.isEmpty()) { // to check if there are any elements in the array list
             System.out.println("No reservations made");
         } else {
             for (int i = 0; i < reservations.size(); i++) {
-                System.out.println("Index " + i + ": " + reservations.get(i));
+                System.out.println("Index " + i + ": " + reservations.get(i)); // prints all elements in reservations
             }
         }
     }
     // Method to find specific reservation and print it
     public void findRes(int i) {
-        if (i >= 0 && i < reservations.size()) {
-            System.out.println("Index " + i + ": " + reservations.get(i));
+        if (i >= 0 && i < reservations.size()) { // checks if index is valid
+            System.out.println("Index " + i + ": " + reservations.get(i)); // prints reservation 
         } else {
-            System.out.println("No reservations found");
+            System.out.println("No reservations found"); // if index is invalid 
         }
     }
 }

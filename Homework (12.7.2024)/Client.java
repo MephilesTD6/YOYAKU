a5d813623c4055a8ea46b26a0c760e2dca487cd2
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-06-12 15:33:53 +0800

    Update Client.java

--- Homework (12.7.2024)/Client.java
+++ Homework (12.7.2024)/Client.java
@@ -1,47 +1,39 @@
-// Avinash Kumar A/L Jayaseelan
-// 24000113
+// Aiman Naim bin Shaiful Zahrin
+// 22005653
 
 import java.util.Scanner;
 
-public class Client {
+public class Client
+{
     private String name;
     private String phoneNumber;
-
+    
     Scanner sc = new Scanner(System.in);
-
-    public Client(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        System.out.println("Created a client object with name of " + name + " and phone number: " + phoneNumber);
-    }
-
-    public Client() {
-        this.name = "";
-        this.phoneNumber = "";
-        System.out.println("Created client object");
-    }
-
-    public void setName() {
-        System.out.print("Enter name: ");
+    
+       public void setName()
+    {
+        System.out.print("\nEnter client name: ");
         name = sc.nextLine();
     }
-
-    public String getName() {
+    
+    public String getName()
+    {
         return name;
     }
 
-    public void setPhoneNumber() {
-        System.out.print("Enter phone number: ");
+    public void setPhoneNumber()
+    {
+        System.out.print("Enter client phone number: ");
         phoneNumber = sc.nextLine();
     }
 
-    public String getPhoneNumber() {
+    public String getPhoneNumber()
+    {
         return phoneNumber;
     }
-
+    
     @Override
     public String toString() {
-        return "Client name: " + name + " Number of person: " + phoneNumber;
+        return "Client name: " + name + ", client phone number: " + phoneNumber;
     }
-
 }

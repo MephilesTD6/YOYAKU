6d61adbe72ad8d6b9fef8c6ae31ee4439fd08b80
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-06-12 15:34:54 +0800

    Update Reservation.java

--- Homework (12.7.2024)/Reservation.java
+++ Homework (12.7.2024)/Reservation.java
@@ -5,57 +5,72 @@ import java.util.Scanner;
 
 public class Reservation {
 
-    private int tableNumber;
-    private int pax;
-
-    private Scanner scanner = new Scanner(System.in);
-
-    public Reservation(int tableNumber, int pax) {
-        this.tableNumber = tableNumber;
-        this.pax = pax;
-        System.out.println("\nCreated a reservation object with table number" + tableNumber + "for" + pax + "persons.");
-
-    }
-
-    public Reservation() {
-        this.tableNumber = 0;
-        this.pax = 0;
-        System.out.println("\nCreated a reservation object");
-    }
-
-    // input table number
-    public void setTable() {
-        System.out.println("Enter table number: ");
+  private int tableNumber;
+  private int pax;
+  private boolean status;
+  private String name;
+  private String phoneNumber;
+  
+   private Scanner scanner = new Scanner(System.in);
+   
+   public Reservation(int tableNumber, int pax, boolean status)
+   {
+       this.tableNumber = tableNumber;
+       this.pax = pax;
+       this.status = false;
+       System.out.println("\nCreated a reservation object with table number" + tableNumber + "for" + pax +"persons.");
+       
+   }
+   public Reservation()
+   {
+       this.tableNumber = 0;
+       this.pax = 0;
+       this.status = false;
+   }
+   
+   //input table number
+      public void setTable()
+    {
+        System.out.print("Enter table number: ");
         this.tableNumber = scanner.nextInt();
-
+        
     }
 
-    // input number of persons
-    public void setPax() {
-        System.out.println("Enter number of pax: ");
+    //input number of persons
+    public void setPax()
+    {
+        System.out.print("Enter number of pax: ");
         this.pax = scanner.nextInt();
     }
-
-    public int getTable() {
-        System.out.println("\nThe table number is: " + tableNumber);
-        return tableNumber;
+    
+   public int getTable()
+   {
+       System.out.print("\nThe table number is: " + tableNumber);
+       return tableNumber;
+   }
+   
+   public int getPax ()
+   {
+       System.out.print("\nThe number of pax are : " + pax);
+       return pax;
+   }
+    
+    public void readClientDetails() {
+        Client aiman = new Client();
+        aiman.setName();
+        aiman.setPhoneNumber();
+        this.name = aiman.getName();
+        this.phoneNumber = aiman.getPhoneNumber();
     }
 
-    public int getPax() {
-        System.out.println("\nThe number of pax are : " + pax);
-        return pax;
+    public void setTableStatus(boolean status) {
+        Table syukri = new Table();
+        this.status = status;
+        syukri.setStatus(status);
     }
-
-    // read input from Client class
-    Client aiman = new Client();
-    String name = aiman.getName();
-    String phoneNumber = aiman.getPhoneNumber();
-
-    // To translate output to human friendly language, if not used, output from
-    // Reservations would be printed as Table@1a2b3c4d
-    @Override
+    
+        @Override
     public String toString() {
-        return "Table Number: " + tableNumber + ", Number of pax: " + pax + ", Client name: " + name
-                + ", Client phone number: " + phoneNumber;
-    }
+        return "Table Number: " + tableNumber + ", Number of pax: " + pax + ", Client name: " + name + ", Client phone number: " + phoneNumber + ", Reservation status: " + status;
+    } 
 }

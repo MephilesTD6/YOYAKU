2b2095a55a4ec39475937d86479f70ec8687573f
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-07-09 11:56:27 +0800

    Update Table.java

--- Homework (9.7.2024)/Table.java
+++ Homework (9.7.2024)/Table.java
@@ -1,172 +1,73 @@
+// Syukri Fadhli bin Ahmad 24000074
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import java.util.Scanner;
 
-public class ClientGUI extends Application {
-    private Clients clients = new Clients();
-    private Reservations reservations = new Reservations();
-    private Tables tables = new Tables();
-    
-    private GridPane createPane() {
-        GridPane pane = new GridPane();
-        pane.setAlignment(Pos.CENTER);
-        pane.setPadding(new Insets(10, 10, 10, 10));
-        pane.setHgap(10);
-        pane.setVgap(10);
-        return pane;
+public class Table {
+
+    private int tableNumber;
+    private int chairNumber;
+    private boolean status = false;
+
+    Scanner sc = new Scanner(System.in);
+
+    // CONSTRUCTORS
+    public Table(int tableNumber, int chairNumber, boolean status) {// object will be created when called with parameters
+        this.tableNumber = tableNumber;// assigned to value of parameters passed through
+        this.chairNumber = chairNumber;
+        this.status = status;
+        System.out.println("\nCreated a Table object with table number of " + tableNumber + " and number of chair " + chairNumber );
     }
 
+    public Table() {// default object will be created
+        this.tableNumber = 0;
+        this.chairNumber = 0;
+        System.out.print("\nCreated a Table object ");
+    }
+
+    // SETTERS
+    public void setTableNum() {// prompt to set table number
+        System.out.print("\nEnter table number: ");
+        tableNumber = sc.nextInt();
+    }
+
+    public void setChairNum() {// prompt to set chair number
+        System.out.print("Enter number of chair: ");
+        chairNumber = sc.nextInt();
+    }
+
+    public void setStatus(boolean status) {// prompt to give status of reservation
+        this.status = status;
+    }
+
+    // GETTERS
+    public int getTableNum() {// return table number when called
+        System.out.print("\nThe table number is: " + tableNumber);
+        return tableNumber;
+    }
+
+    public int getChairNum() {// return chair number when called
+        System.out.print("\nThe number of chairs is: " + chairNumber);
+        return chairNumber;
+    }
+
+    public boolean getStatus() {// return status of reservation
+        return status;
+    }
+
+    // To translate output to human friendly language, if not used, output from
+    // Tables would be printed as Table@1a2b3c4d
+    /*
+     * Another way to code would to be:
+     * 
+     * public String printDetails() {
+     * return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber;
+     * }
+     * 
+     * then, when calling ArrayList actions: use for example,
+     * seetho.get(indexNumber).printDetails();
+     */
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Client Manager");
-
-        GridPane mainPane = createPane();
-        GridPane addClientPane = createPane();
-        GridPane removeClientPane = createPane();
-        GridPane printClientPane = createPane();
-        GridPane findClientPane = createPane();
-
-        // Main Scene Buttons
-        Button addClientBtn = new Button("Add Client");
-        Button removeClientBtn = new Button("Remove Client");
-        Button printClientBtn = new Button("Print Client");
-        Button findClientBtn = new Button("Find Client");
-
-        // Set preferred size
-        int btnHeight = 50;
-        int btnWidth = 150;
-        addClientBtn.setPrefSize(btnWidth , btnHeight);
-        removeClientBtn.setPrefSize(btnWidth, btnHeight);
-        printClientBtn.setPrefSize(btnWidth, btnHeight);
-        findClientBtn.setPrefSize(btnWidth, btnHeight);
-        
-        // Add buttons to the main pane
-        mainPane.add(addClientBtn, 0, 0);
-        mainPane.add(removeClientBtn, 0, 1);
-        mainPane.add(printClientBtn, 0, 2);
-        mainPane.add(findClientBtn, 0, 3);
-
-        Scene mainScene = new Scene(mainPane, 300, 200);
-
-        // Add Client Pane
-        Button backFromAddClientBtn = new Button("Back");
-        Label nameLabel = new Label("Name:");
-        TextField nameField = new TextField();
-        Label phoneLabel = new Label("Phone Number:");
-        TextField phoneField = new TextField();
-        Button okBtn = new Button("OK");
-        Button cancelBtn = new Button("Cancel");
-
-        addClientPane.add(backFromAddClientBtn, 0, 0);
-        addClientPane.add(nameLabel, 0, 1);
-        addClientPane.add(nameField, 1, 1);
-        addClientPane.add(phoneLabel, 0, 2);
-        addClientPane.add(phoneField, 1, 2);
-        addClientPane.add(okBtn, 0, 3);
-        addClientPane.add(cancelBtn, 1, 3);
-
-        Scene addClientScene = new Scene(addClientPane, 300, 200);
-
-        // Remove Client Pane
-        Button backFromRemoveClientBtn = new Button("Back");
-        Label indexLabelRemove = new Label("Enter Index:");
-        TextField indexFieldRemove = new TextField();
-        Button okBtnRemove = new Button("Ok");
-
-        removeClientPane.add(backFromRemoveClientBtn, 0, 0);
-        removeClientPane.add(indexLabelRemove, 0, 1);
-        removeClientPane.add(indexFieldRemove, 1, 1);
-        removeClientPane.add(okBtnRemove, 0,2);
-
-        Scene removeClientScene = new Scene(removeClientPane, 300, 200);
-
-        // Print Client Pane
-        Button backFromPrintClientBtn = new Button("Back");
-        Label statusPrint = new Label("");
-        Button okBtnPrint = new Button("Print");
-
-        printClientPane.add(backFromPrintClientBtn, 0, 0);
-        printClientPane.add(okBtnPrint, 1, 0);
-        printClientPane.add(statusPrint, 0, 1);
-
-        Scene printClientScene = new Scene(printClientPane, 300, 200);
-
-        // Find Client Pane
-        Button backFromFindClientBtn = new Button("Back");
-        Label indexLabelFind = new Label("Enter Index:");
-        TextField indexFieldFind = new TextField();
-        Button okBtnFind = new Button("Ok");
-
-
-        findClientPane.add(backFromFindClientBtn, 0, 0);
-        findClientPane.add(indexLabelFind, 0, 1);
-        findClientPane.add(indexFieldFind, 1, 1);
-        findClientPane.add(okBtnFind, 0, 2);
-
-        Scene findClientScene = new Scene(findClientPane, 300, 200);
-
-        
-        //Setting up button actions
-         
-        //Main 4 button actions
-        addClientBtn.setOnAction(e -> {primaryStage.setScene(addClientScene);});
-        removeClientBtn.setOnAction(e -> {primaryStage.setScene(removeClientScene);});
-        printClientBtn.setOnAction(e -> {primaryStage.setScene(printClientScene);});
-        findClientBtn.setOnAction(e -> {primaryStage.setScene(findClientScene);});
-        
-        //Add client actions
-        backFromAddClientBtn.setOnAction(e -> {primaryStage.setScene(mainScene);});
-        
-        cancelBtn.setOnAction(e -> {
-            nameField.setText("");
-            phoneField.setText("");
-        });
-        
-        okBtn.setOnAction(e -> {
-            String name = nameField.getText();
-            String phone = phoneField.getText();
-            
-            Client newClient = new Client (name, phone);
-            clients.addClient(newClient);
-            //clients.printClient();
-        });
-
-        
-        //Remove client actions
-        backFromRemoveClientBtn.setOnAction(e -> {primaryStage.setScene(mainScene);});
-        
-        okBtnRemove.setOnAction(e -> {
-            int clientIndex = Integer.parseInt(indexFieldRemove.getText());
-            clients.removeClient(clientIndex);
-            //clients.printClient();
-        });
-        
-        //Print client actions
-        backFromPrintClientBtn.setOnAction(e -> {primaryStage.setScene(mainScene);});
-        
-        okBtnPrint.setOnAction(e -> {
-           clients.printClient(); 
-           statusPrint.setText("Successful");
-        });
-        
-        //Find client actions
-        backFromFindClientBtn.setOnAction(e -> {primaryStage.setScene(mainScene);});
-        
-        okBtnFind.setOnAction(e -> {
-            int clientIndex = Integer.parseInt(indexFieldFind.getText());
-            clients.findClient(clientIndex);
-        });
-        
-        
-        //set scene into stage for visibility
-        primaryStage.setScene(mainScene);
-        primaryStage.show();
-    }    
+    public String toString() {
+        return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber + ", Reservation status: " + status;
+    }
 }

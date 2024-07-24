195801b1a100e7cc0153eee62a13bbdccccc6c6b
MephilesTD6 <162244895+MephilesTD6@users.noreply.github.com>
2024-07-24 13:45:40 +0800

    Create Table.java

---
+++ Homework (26.7.2024)/Table.java
@@ -0,0 +1,73 @@
+// Syukri Fadhli bin Ahmad 24000074
+
+import java.util.Scanner;
+
+public class Table {
+
+    private int tableNumber;
+    private int chairNumber;
+    private boolean status = false;
+
+    Scanner sc = new Scanner(System.in);
+
+    // CONSTRUCTORS
+    public Table(int tableNumber, int chairNumber, boolean status) {// object will be created when called with parameters
+        this.tableNumber = tableNumber;// assigned to value of parameters passed through
+        this.chairNumber = chairNumber;
+        this.status = status;
+        System.out.println("\nCreated a Table object with table number of " + tableNumber + " and number of chair " + chairNumber );
+    }
+
+    public Table() {// default object will be created
+        this.tableNumber = 0;
+        this.chairNumber = 0;
+        System.out.print("\nCreated a Table object ");
+    }
+
+    // SETTERS
+    public void setTableNum() {// prompt to set table number
+        System.out.print("\nEnter table number: ");
+        tableNumber = sc.nextInt();
+    }
+
+    public void setChairNum() {// prompt to set chair number
+        System.out.print("Enter number of chair: ");
+        chairNumber = sc.nextInt();
+    }
+
+    public void setStatus(boolean status) {// prompt to give status of reservation
+        this.status = status;
+    }
+
+    // GETTERS
+    public int getTableNum() {// return table number when called
+        System.out.print("\nThe table number is: " + tableNumber);
+        return tableNumber;
+    }
+
+    public int getChairNum() {// return chair number when called
+        System.out.print("\nThe number of chairs is: " + chairNumber);
+        return chairNumber;
+    }
+
+    public boolean getStatus() {// return status of reservation
+        return status;
+    }
+
+    // To translate output to human friendly language, if not used, output from
+    // Tables would be printed as Table@1a2b3c4d
+    /*
+     * Another way to code would to be:
+     * 
+     * public String printDetails() {
+     * return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber;
+     * }
+     * 
+     * then, when calling ArrayList actions: use for example,
+     * seetho.get(indexNumber).printDetails();
+     */
+    @Override
+    public String toString() {
+        return "Table Number: " + tableNumber + ", Chair Number: " + chairNumber + ", Reservation status: " + status;
+    }
+}
